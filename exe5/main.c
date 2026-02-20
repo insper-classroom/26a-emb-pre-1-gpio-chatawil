88e987e5d561b4bc4ae95a9bde6ffaac2d7e7142
chatawil <charlesht@al.insper.edu.br>
2026-02-20 14:47:31 -0300

    Ex 5

--- exe5/main.c
+++ exe5/main.c
@@ -23,9 +23,14 @@ int main() {
         if (!gpio_get(BTN_PIN)) {
             printf("Botao 1: %d\n", cnt_1++);
         }
+            while (!gpio_get(BTN_PIN));
+            sleep_ms(20);                // debounce
+
 
         if (!gpio_get(BTN_PIN_2)) {
             printf("Botao 2: %d\n", cnt_2++);
         }
+            while (!gpio_get(BTN_PIN_2));     
+            sleep_ms(20);                // debounce
     }
 }
