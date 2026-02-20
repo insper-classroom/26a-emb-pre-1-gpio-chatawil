ce29bce6028db339dc9c0a85b13fed8173058a9a
chatawil <charlesht@al.insper.edu.br>
2026-02-20 09:40:10 -0300

    Ex1 e 2

--- exe1/main.c
+++ exe1/main.c
@@ -3,17 +3,22 @@
 #include <stdio.h>
 
 const int LED_PIN_R = 5;
+const int LED_PIN_A = 28;
 
 int main() {
   stdio_init_all();
 
   gpio_init(LED_PIN_R);
+  gpio_init(LED_PIN_A);
   gpio_set_dir(LED_PIN_R, GPIO_OUT);
+  gpio_set_dir(LED_PIN_A, GPIO_OUT);
 
   while (true) {
     gpio_put(LED_PIN_R, 1);
+    gpio_put(LED_PIN_A, 1);
     sleep_ms(250);
 
+    gpio_put(LED_PIN_A, 0);
     gpio_put(LED_PIN_R, 0);
     sleep_ms(250);
   }
